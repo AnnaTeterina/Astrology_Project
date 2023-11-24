b6abf74d892fdf9e5a37cb090aa0f826e9db24dc
Anna <107703043+AnnaTeterina@users.noreply.github.com>
2023-11-24 15:01:44 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 let buttonArticles = document.getElementById("buttonArticles")
 let buttonMeditations = document.getElementById("buttonMeditations")
-let articles_block = document.getElementById("articles")
+let articles_block = document.getElementById("swiper")
+let meditations_block = document.getElementById("meditations")
 
 function clickedButtonAsrticles() { 
     buttonArticles.classList.add("button_clicked");
-    buttonMeditations.classList.remove("button_clicked")
-    articles_block.classList.remove("non_active")
+    buttonMeditations.classList.remove("button_clicked");
+    //meditations_block.classList.add("non_active")
+    //articles_block.classList.remove("non_active");
+    articles_block.style.display = 
 }
 
 function clickedButtonMeditations() { 
     buttonMeditations.classList.add("button_clicked");
-    buttonArticles.classList.remove("button_clicked")
-    articles_block.classList.add("non_active")
+    buttonArticles.classList.remove("button_clicked");
+    //articles_block.classList.add("non_active");
+    //meditations_block.classList.remove("non_active")
 }
 
